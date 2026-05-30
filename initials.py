f42284a79cb331d8ad1b33802429484d660df2e6
NanaYaa-Acheampong <2425401435@live.gctu.edu.gh>
2026-05-30 18:56:43 +0000

    Update initials.py
    Assignment 2

--- initials.py
+++ initials.py
@@ -1,7 +1,28 @@
-
 def main():
-  pass #code goes here
 
+#File: NYSA.py
+#Description: Print out my initials.
+#Assignment Number: 1
+#
+#Name: Nana Yaa Serwaa Acheampong
+#STUDENT ID: 2425401435
+#Email: nayaacheampong06@gmail.com
+#Grader: Augustus Buckman
+#
+#On my honor, Augustus Buckman, this programming assignment is my own work
+#and I have not provided this code to any other students.
 
+print("...NYSA")
+
+print("...NN........NN...YY........YY.....SSSSSSSSSS............AA..........")
+print("...NNNN......NN....YY......YY....SSSSSSSSSSSS.........AA...AA........")
+print("...NN.NN.....NN.....YY....YY....SSSS.................AA.....AA.......")
+print("...NN.NN.....NN......YY..YY.....SSSS................AA.......AA......")
+print("...NN..NN....NN.......Y..Y........SSSS.............AA.........AA.....")
+print("...NN...NN...NN........YY..........SSSSS..........AAAAAAAAAAAAAAA....")
+print("...NN....NN..NN........YY..............SSSSS.....AA.............AA...")
+print("...NN.....NN.NN........YY...............SSSS....AA................AA.")
+print("...NN......NNNN........YY........SSSSSSSSSSS...AA.................AA.")
+print("...NN........NN........YY........SSSSSSSSSSS..AA...................AA")
 
 main()
